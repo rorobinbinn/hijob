864f2e96f59393baaa57f56da114441b09ba9a5f
Robin <robin071196@gmail.com>
2020-05-14 16:21:41 +0700

    Initial Commit

---
+++ client/src/Routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
